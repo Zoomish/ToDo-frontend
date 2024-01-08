d0db0773d891ba076adfb6e78177d7e77f8ce9f2
Zoom <egor147536987@gmail.com>
2024-01-08 16:47:29 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,82 +1,101 @@
 import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
-import { getAllToDo,addToDo, updateToDo,deleteToDo } from "./utils/HandleApi";
-
+import { getAllToDo, addToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
 
 function App() {
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [description, setDescription] = useState("");
+  const [type, setType] = useState(1);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [toDoId, setToDoId] = useState("");
 
-  const [toDo, setToDo]=useState([])
-  const [text,setText]=useState('')
-  const [description, setDescription]=useState('')
-  const [type, setType]=useState(1)
-  const [isUpdating, setIsUpdating]=useState(false)
-  const [toDoId,setToDoId]=useState('')
+  useEffect(() => {
+    getAllToDo(setToDo);
+  }, []);
 
+  const updateMode = (_id, text, description, type, setIsUpdating) => {
+    setIsUpdating(true);
+    setText(text);
+    setDescription(description);
+    setType(type);
+    setToDoId(_id);
+  };
 
-  useEffect(()=>{
-    getAllToDo(setToDo)
-},[])
-
-  const updateMode=(_id,text,description,type,setIsUpdating)=>{
-    setIsUpdating(true)
-    setText(text)
-    setDescription(description)
-    setType(type)
-    setToDoId(_id)
-  }
-
-  const setNull=()=>{
-    setText('')
-    setDescription('')
-    setType(1)
-    setIsUpdating(false)
-
-  }
+  const setNull = () => {
+    setText("");
+    setDescription("");
+    setType(1);
+    setIsUpdating(false);
+  };
 
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <h1>ToDo App</h1>
-        <div className='top'>
-          <input 
-          type="text" 
-          placeholder='Enter Task' 
-          value={text}
-          onChange={(e)=>setText(e.target.value)}
-          />
-          <input 
-          type="text" 
-          placeholder='Enter description' 
-          value={description}
-          onChange={(e)=>setDescription(e.target.value)}
-          />
-          <input 
-          type="text" 
-          placeholder='Enter Type' 
-          value={type}
-          onChange={(e)=>setType(e.target.value)}
-          />
-          <div 
-          className="add" 
-          onClick={isUpdating?()=>updateToDo(toDoId, setToDo,text,description, type, setNull, setIsUpdating):()=>addToDo(text,description,type,setNull,setToDo)
-          }>
-            {isUpdating?'Update':'Add'}
+        <div className="top">
+          <div className="inputes">
+            <input
+              type="text"
+              placeholder="Enter Task"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Enter description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Enter Type"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            />
+          </div>
+
+          <div
+            className="add"
+            onClick={
+              isUpdating
+                ? () =>
+                    updateToDo(
+                      toDoId,
+                      setToDo,
+                      text,
+                      description,
+                      type,
+                      setNull,
+                      setIsUpdating
+                    )
+                : () => addToDo(text, description, type, setNull, setToDo)
+            }
+          >
+            {isUpdating ? "Update" : "Add"}
           </div>
         </div>
         <div className="list">
-          {toDo.map((item)=>
-            <ToDo 
-            key={item._id} 
-            text={item.text}
-            description={item.description}
-            type={item.type}
-            updateToDo={()=>updateMode(item._id, item.text,item.description, item.type,setIsUpdating)}
-            deleteToDo={()=>deleteToDo(item._id,setToDo)}
+          {toDo.map((item) => (
+            <ToDo
+              key={item._id}
+              text={item.text}
+              description={item.description}
+              type={item.type}
+              updateToDo={() =>
+                updateMode(
+                  item._id,
+                  item.text,
+                  item.description,
+                  item.type,
+                  setIsUpdating
+                )
+              }
+              deleteToDo={() => deleteToDo(item._id, setToDo)}
             />
-          )}
+          ))}
         </div>
       </div>
-
     </div>
   );
 }
